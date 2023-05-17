87efb24ae7f2303d718a1aba016cb841c1c4d6f7
Attila Zsolt Danku <110828017+attilazsoltdanku@users.noreply.github.com>
2023-05-17 15:05:09 +0200

    Revert "Chromium-package"

--- stacks/ExampleStack.ts
+++ stacks/ExampleStack.ts
@@ -5,14 +5,10 @@ export function ExampleStack({ stack }: StackContext) {
   const api = new Api(stack, "Api", {
     routes: {
       "GET /simple": "packages/functions/src/simple-lambda.handler",
-      "GET /long-execution-time":
-        "packages/functions/src/get-long-execution-time.handler",
-      "GET /long-cold-start":
-        "packages/functions/src/get-long-cold-start.handler",
+      "GET /long-execution-time": "packages/functions/src/get-long-execution-time.handler",
+      "GET /long-cold-start": "packages/functions/src/get-long-cold-start.handler",
       "GET /test": "packages/functions/src/test.handler",
       "GET /test2": "packages/functions/src/test2.handler",
-      "GET /over-three-sec":
-        "packages/functions/src/get-over-three-sec.handler",
     },
   });
 
